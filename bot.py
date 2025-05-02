7fdd0b733d9e91ddb15b827689b41fa2a9461a50
yahyalahlou <132085241+yahyalahlou@users.noreply.github.com>
2025-05-02 06:57:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,24 +3,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 import requests
 import os
 
-# Setup Telegram bot credentials
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-# Google Sheet details
 SHEET_NAME = "WinxyBot - Bet Feed"
 WORKSHEET_NAME = "Sheet1"
 
-# Load Google credentials from file
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("winxybot-gsheetaccess-90658a4ae6f6.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("winxybot-gsheetaccess-cee2b3cd0651.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open(SHEET_NAME).worksheet(WORKSHEET_NAME)
 
-# Fetch all rows
 rows = sheet.get_all_records()
 
-# Process each row
 for idx, row in enumerate(rows, start=2):
     if str(row["Active"]).upper() == "TRUE" and str(row["Alert Sent?"]).upper() != "TRUE":
         message = (
@@ -45,4 +40,4 @@ for idx, row in enumerate(rows, start=2):
         }
 
         requests.post(url, json=payload)
-        sheet.update_cell(idx, 8, "TRUE")  # Update 'Alert Sent?' to TRUE
+        sheet.update_cell(idx, 8, "TRUE")
