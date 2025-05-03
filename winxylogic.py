1cbf4dbfd41a53f6ffcdea5ed45542afc74042f7
yahyalahlou <132085241+yahyalahlou@users.noreply.github.com>
2025-05-03 14:42:46 +0100

    Update winxylogic.py

--- winxylogic.py
+++ winxylogic.py
@@ -1,4 +1,7 @@
-def calculate_winxy_confidence(scraped_data, **kwargs):
+def calculate_winxy_confidence(scraped_data=None, **kwargs):
+    if scraped_data is None:
+        scraped_data = {}
+
     # Base score
     confidence = 50
 
